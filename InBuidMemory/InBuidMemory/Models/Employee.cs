86f1351456524c71549e1a014140ec4f1a897734
Md. Sifat Hossain <17204034@student.duet.ac.bd>
2021-10-02 00:53:20 +0600

    web API using inbuild memory

---
+++ InBuidMemory/InBuidMemory/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InBuidMemory.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+      
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Zipcode { get; set; }
+    }
+}
